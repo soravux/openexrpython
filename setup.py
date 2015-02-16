26084f1093d4ff8c312406dfc9e5bef5103584fc
Yannick Hold <yannickhold@gmail.com>
2015-02-16 13:25:55 +0000

    Switched to setuptools to support develop

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from distutils.command.build_py import build_py as _build_py
+from setuptools import setup
+from setuptools.extension import Extension
+from setuptools.command.build_py import build_py as _build_py
 
 from os import system
 
@@ -14,7 +14,7 @@ setup(name='OpenEXR',
   description = "Python bindings for ILM's OpenEXR image file format",
   long_description = "Python bindings for ILM's OpenEXR image file format",
   version=version,
-  ext_modules=[ 
+  ext_modules=[
     Extension('OpenEXR',
               ['OpenEXR.cpp'],
               include_dirs=['/usr/include/OpenEXR', '/usr/local/include/OpenEXR', '/opt/local/include/OpenEXR'],
